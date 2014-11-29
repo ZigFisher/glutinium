cabad1d3ba4e3e68f4c7b8c8720edc0aa7609cb1
Zig Fisher <flyrouter@gmail.com>
2014-11-29 21:38:27 +0300

    Update

--- fixsum/src/fixsum.c
+++ fixsum/src/fixsum.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * (C) Copyright 2010
  * evaxige <evxige at gmail dot com>
  * Version:	1.0
@@ -7,12 +7,12 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version
- * 
+ *
  */
 
 #include <stdio.h>
@@ -21,7 +21,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+//#include <unistd.h>
 
 static uint16_t swab16(uint16_t x)
 {
@@ -35,8 +35,8 @@ static void parse_mac(char* str, uint8_t* mac)
     char* tp = NULL;
     char* buf = str;
     int i = 0;
- 
-    while( NULL != (tp = strtok(buf, ":")) ) {
+
+    while( NULL != (tp = strtok(buf, ":-")) ) {
         if(i < 6 && tp) {
             mac[i] = (uint8_t)(strtol(tp, NULL, 16));
 /*            printf("0x%02x\n", mac[i]);*/
@@ -45,6 +45,30 @@ static void parse_mac(char* str, uint8_t* mac)
         buf = NULL;
     }
 }
+
+static void printmac(char* str, uint8_t* mac)
+{
+    int i = 0;
+	printf("%s",str);
+    while( i<6 ) {
+       printf("%02x", mac[i]);
+	   if (i<5) printf(":");
+       i++;
+      }
+	printf("\n");
+}
+
+static int checkmac(uint8_t* mac)
+{
+	int i = 0;
+	i=0;
+	if ((mac[0]==00) && (mac[1]==0x15) && (mac[2]==0x6d)) { i=1; }
+	if ((mac[0]==00) && (mac[1]==0x27) && (mac[2]==0x22)) { i=1; }
+	if ((mac[0]==0xDC) && (mac[1]==0x9F) && (mac[2]==0xDB)) { i=1; }
+	if ((mac[0]==0x24) && (mac[1]==0xA4) && (mac[2]==0x3C)) { i=1; }
+	return i;
+}
+
 
 static void modify_mac(uint8_t *data, uint8_t *mac)
 {
@@ -83,11 +107,26 @@ static void modify_mac(uint8_t *data, uint8_t *mac)
 	}
 }
 
+static void modify_mac_lan(uint8_t *data, uint8_t *mac)
+{
+	int i = 0;
+	printf("modify mac lan ");
+	/* modify mac */
+	for(i = 0; i < 6; i++) {
+		data[i] = mac[i];
+		printf("%02x", mac[i]);
+		if (i<5) { printf(":"); }
+	}
+	printf("\n");
+
+}
+
+
 static int save_data(uint8_t *data, int len)
 {
 	FILE* pf = NULL;
 
-	if( NULL == (pf = fopen("new_falsh.bin", "wb")) ) {
+	if( NULL == (pf = fopen("EEPROM_NEW.bin", "wb")) ) {
 		perror("fopen");
 		return -1;
 	}
@@ -107,16 +146,18 @@ void usage()
 	printf("Fix atheros ath9k chipset EEPROM checksum\n");
 	printf("Binary file is wholeflash data or \"EEPROM\" partition\n");
 	printf("Usage:\n");
-	printf("./fixsum filename org_mac new_mac\n");
-	printf("Example:\n./fixsum flash.bin 01:02:03:04:05:06 11:22:33:44:55:66\n");
+	printf("fixsum filename org_mac new_mac\n");
+	printf("Example:\nfixsum /tmp/EEPROM_ORIG.bin 01:02:03:04:05:06 11:22:33:44:55:66\n");
 }
 
 int main(int argc, char* argv[])
 {
+	int j = 0;
 	int i = 0;
 	int file_len = 0;
 	uint8_t *data = NULL;
 	uint8_t old_mac[6] = {0}, new_mac[6] = {0};
+	uint8_t new_mac2[6] = {0}, new_mac3[6] = {0};
 	FILE* pf = NULL;
 	struct stat st;
 
@@ -127,7 +168,23 @@ int main(int argc, char* argv[])
 
 	/* parse arguments */
 	parse_mac(argv[2], old_mac);
+	printmac("Old ",old_mac);
 	parse_mac(argv[3], new_mac);
+	printmac("New ",new_mac);
+    if (checkmac(new_mac)==0) {
+		perror("mac ungenuine");
+		exit(-1);
+	}
+	
+	i=0;
+    while( i<6 ) {
+	   new_mac2[i]=new_mac[i];
+	   new_mac3[i]=new_mac[i];
+       i++;
+      }
+	 new_mac2[3]=new_mac[3]+1; 
+	 new_mac3[3]=new_mac[3]+1; 
+	 new_mac3[0]=new_mac[0]+2; 
 
 	/* read file */
 	if(-1 == stat(argv[1], &st)) {
@@ -153,28 +210,58 @@ int main(int argc, char* argv[])
 	}
 	fclose(pf);
 
+	printf("HWID: %02x%02x\n", data[0x1016],data[0x1017]);
+
 	/* find offset */
 	for(i = 0; i < file_len; i++) {
 		if( old_mac[0] == data[i + 0] &&
-			old_mac[1] == data[i + 1] &&
+		    old_mac[1] == data[i + 1] &&
 			old_mac[2] == data[i + 2] &&
 			old_mac[3] == data[i + 3] &&
 			old_mac[4] == data[i + 4] &&
 			old_mac[5] == data[i + 5] ) {
-
-			printf("find old_mac offset(0x%x)\n", i);
-
+			printf("find old_mac_wifi offset(0x%x)\n", i);
 			/* update new mac */
-			modify_mac(data + (i - 0xc), new_mac);
-
-			if(0 == save_data(data, file_len)) {
-				printf("change mac ok!\n");
+			if (i>0x8000) {
+			  modify_mac_lan(data + i, new_mac);
 			}
+			else {
+			  modify_mac(data + (i - 0xc), new_mac);
+			}
+			j++;
+		}
+		if( old_mac[0] == data[i + 0] &&
+		    old_mac[1] == data[i + 1] &&
+			old_mac[2] == data[i + 2] &&
+			(old_mac[3]+0x1) == data[i + 3] &&
+			old_mac[4] == data[i + 4] &&
+			old_mac[5] == data[i + 5] ) {
+			printf("find old_mac_lan offset(0x%x)\n", i);
+			/* update new mac lan 2*/
+			modify_mac_lan(data + i, new_mac2);
+			j++;
+		}
 
-			break;
+		if( (old_mac[0]+2) == data[i + 0] &&
+		    old_mac[1] == data[i + 1] &&
+			old_mac[2] == data[i + 2] &&
+			(old_mac[3]+0x1) == data[i + 3] &&
+			old_mac[4] == data[i + 4] &&
+			old_mac[5] == data[i + 5] ) {
+			printf("find old_mac_lan2 offset(0x%x)\n", i);
+			/* update new mac lan 2*/
+			modify_mac_lan(data + i, new_mac3);
+			j++;
 		}
 	}
+    
+	if (j>0) 
+	if (0 == save_data(data, file_len)) {
+				printf("change %04x mac-s ok!\n",j);
+			}
 
+	
+	
 	free(data);
 	
 	return 0;
