a468f36243d2e589d4ae0f21a50f241638b38cd6
Zig Fisher <flyrouter@gmail.com>
2016-10-05 21:38:36 +0300

    Update little-wire

--- little-wire/src/littleWire_util.c
+++ little-wire/src/littleWire_util.c
@@ -5,8 +5,10 @@
 void delay(unsigned int duration)
 {
 	#ifdef LINUX
+		// use standard unix api with microseconds
 		usleep(duration*1000);
 	#else
-		Sleep(duration);
+		// Sleep(duration);
+		Sleep(duration * 2);
 	#endif
 }
