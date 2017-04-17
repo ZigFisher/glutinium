61709adfb79390cbfe83bb4dbe1e92ff4808bcb1
Zig Fisher <flyrouter@gmail.com>
2017-04-17 22:34:08 +0300

    Update

--- flyscripts/files/usr/sbin/monitor.sh
+++ flyscripts/files/usr/sbin/monitor.sh
@@ -7,5 +7,6 @@ MACID=`ifconfig eth0 | grep HWaddr | awk '{FS=" "; if(NR==1) {print $5}};' | tr 
 WANIP=`ifconfig br-lan | grep 'Mask:' | tr ':' ' ' | awk '{print $3}' | head -n 1`
 MEMORY=`free | awk '/Mem:/ {print $4}' | egrep '^[-+]?[0-9]*\.?[0-9]+$'`
 UPTIME=`cat /proc/uptime | cut -d '.' -f 1 | egrep '^[-+]?[0-9]*\.?[0-9]+$'`
+ROUTE=`ip r | awk -F ' ' '/default/ {print $3}' | awk -F 'wan' '/3g/ {print $2}'`
 
-httping -q -G -c 1 -t 5 -g "$CLOUD?host=$NAME&mac=$MACID&wanip=$WANIP&freemem=$MEMORY&uptime=$UPTIME"
+httping -q -G -c 1 -t 5 -g "$CLOUD?host=$NAME&mac=$MACID&wanip=$WANIP&freemem=$MEMORY&uptime=$UPTIME&gpio0=$ROUTE"
