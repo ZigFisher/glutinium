7fc39f2a5f71f8d48e3f74bbe6deaf33fa91b289
Zig Fisher <flyrouter@gmail.com>
2017-05-04 14:27:21 +0300

    Update

--- flyscripts/files/usr/sbin/test_oko_20161206.sh
+++ flyscripts/files/usr/sbin/test_oko_20161206.sh
@@ -1,14 +1,17 @@
 
+# Push data to FlyPeek service
 
-(usbgpio mode 7 1; while true ; do sleep 1; /usr/sbin/oko.sh >/dev/null 2>&1; done) &
+usbgpio mode 7 1
+(while true ; do sleep 1; /usr/sbin/oko.sh >/dev/null 2>&1; done) &
 
+**********************************************************************************
 
 #!/bin/sh
 #
 # Create snapshot via radar search
 
-
 if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
   logger -t service_flypeek "Radar is active !"
-  /usr/sbin/service_flypeek_usb.sh
+  # /usr/sbin/service_flypeek_usb.sh
+  /usr/sbin/service_flypeek_ipc.sh
 fi
