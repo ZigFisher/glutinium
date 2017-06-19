1d41afd58f9e2d34c5bbfc903c4054fc57cfe30b
Zig Fisher <flyrouter@gmail.com>
2017-06-20 00:21:22 +0300

    Update

--- flyscripts/files/usr/sbin/oko.sh
+++ flyscripts/files/usr/sbin/oko.sh
@@ -3,6 +3,6 @@
 # Create snapshot via radar search
 
 if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
-  logger -t service_flypeek "Radar is active !"
+  logger -t flypeek "Radar is active !"
   /usr/sbin/service_flypeek.sh
 fi
