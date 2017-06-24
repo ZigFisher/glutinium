4a423347a509bf4fef2dda386849df752bef3751
Zig Fisher <flyrouter@gmail.com>
2017-06-24 12:07:31 +0300

    Update

--- flyscripts/files/usr/sbin/oko.sh
+++ flyscripts/files/usr/sbin/oko.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 #
-# Create snapshot via radar search
+# Scan home via Radar detector
+# Send SMS via AlphaSMS service
+# Create snapshot from IPCam
+
+SERVICE="https://alphasms.ua/api/http.php?version=http&command=send"
+LOGIN= "+380670000000"
+PASSWORD="mypass"
+FROM="Test"
+CLIENT="+380500000000"
+MESSAGE="AlarmVision"
+DELAY="60"
+
 
 if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
-  logger -t flypeek "Radar is active !"
+  logger -t flypeek "RadarDetect !"
+  #
+  last=`cat /tmp/.smsdelay`
+  curr=`date +%s`
+  [ $(($curr-$last)) -gt ${DELAY} ] && \
+    curl -k "${SERVICE}&login=${LOGIN}&password=${PASSWORD}&from=${FROM}&to=${CLIENT}&message=${MESSAGE}" | logger -t alphasms && date +%s >/tmp/.smsdelay
+  #
   /usr/sbin/service_flypeek.sh
 fi
