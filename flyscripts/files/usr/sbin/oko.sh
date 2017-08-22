d48d98da1010322f88d2fffb437f9724d732d883
Zig Fisher <flyrouter@gmail.com>
2017-08-22 16:50:53 +0300

    Update

--- flyscripts/files/usr/sbin/oko.sh
+++ flyscripts/files/usr/sbin/oko.sh
@@ -8,14 +8,14 @@ IPCAM='http://192.168.1.10/webcapture.jpg?command=snap&channel=1'
 TOKEN='367706172:AAHqXLtPAUExiF_CUPe42SvrK9b7csi953A'
 RUPOR='@Bricket1'
 ALARM='RadarDetect!'
-YPASS=''
+YPASS='user:pass'
 DELAY='60'
 SLEEP='1'
 
 
 if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
   STAMP=`date +%Y%m%d%H%M%S`
-  logger -t flypeek "${ALARM}"
+  logger -t oko "${ALARM}"
   #
   last=`cat /tmp/.delay`
   curr=`date +%s`
@@ -25,11 +25,13 @@ if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
   # Send photo to Telegram channel
   ( curl -s -k ${IPCAM} -o /tmp/${STAMP}.jpg && \
     curl -s -k -X POST "https://api.telegram.org/bot${TOKEN}/sendPhoto?chat_id=${RUPOR}" -H "Content-Type: multipart/form-data" -F "photo=@/tmp/${STAMP}.jpg" -F "caption=TimeStamp: ${STAMP}" && \
+    logger -t oko "TX to Telegram OK" && \
     rm -f /tmp/${STAMP}.jpg \
   ) &
   # Send photo to Yandex.Disk
   #( curl -s -k ${IPCAM} -o /tmp/${STAMP}.jpg && \
   #  curl -s -k -u "${YPASS}" -T "/tmp/${STAMP}.jpg" https://webdav.yandex.ru/Public/${STAMP}.jpg && \
+  #  logger -t oko "TX to Yandex.Disk OK" && \
   #  rm -f /tmp/${STAMP}.jpg \
   #) &
   #
