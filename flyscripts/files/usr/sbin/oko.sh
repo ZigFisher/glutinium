47d1dcfca269baa7672054054289d12b9f93c791
Zig Fisher <flyrouter@gmail.com>
2017-08-22 16:11:03 +0300

    Update

--- flyscripts/files/usr/sbin/oko.sh
+++ flyscripts/files/usr/sbin/oko.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #
 # Scan home via Radar detector
-# Send Message via Telegram
-# Create snapshot from IPCam
-# Send Photo via Telegram
+# Grab snapshot from connected IPCam
+# Send Message and Photo via Telegram
 
-IPCAM='http://172.28.158.102/webcapture.jpg?command=snap&channel=1'
+IPCAM='http://192.168.1.10/webcapture.jpg?command=snap&channel=1'
 TOKEN='367706172:AAHqXLtPAUExiF_CUPe42SvrK9b7csi953A'
 RUPOR='@Bricket1'
 ALARM='RadarDetect!'
-MEDIA='Content-Type: multipart/form-data'
+YPASS=''
 DELAY='60'
 SLEEP='1'
 
 
 if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
+  STAMP=`date +%Y%m%d%H%M%S`
   logger -t flypeek "${ALARM}"
   #
   last=`cat /tmp/.delay`
@@ -22,9 +22,17 @@ if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
   [ $(($curr-$last)) -gt ${DELAY} ] && \
     curl -s -k -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${RUPOR}&text=${ALARM}" && date +%s >/tmp/.delay
   #
-  ( STAMP=`date +%Y%m%d%H%M%S` && \
-    curl -s -k ${IPCAM} -o /tmp/${STAMP}.jpg && \
-    curl -s -k -X POST "https://api.telegram.org/bot${TOKEN}/sendPhoto?chat_id=${RUPOR}" -H "${MEDIA}" -F "photo=@${PHOTO}" -F "caption=TimeStamp: ${STAMP}" && \
+  # Send photo to Telegram channel
+  ( curl -s -k ${IPCAM} -o /tmp/${STAMP}.jpg && \
+    curl -s -k -X POST "https://api.telegram.org/bot${TOKEN}/sendPhoto?chat_id=${RUPOR}" -H "Content-Type: multipart/form-data" -F "photo=@/tmp/${STAMP}.jpg" -F "caption=TimeStamp: ${STAMP}" && \
     rm -f /tmp/${STAMP}.jpg \
   ) &
+  # Send photo to Yandex.Disk
+  #( curl -s -k ${IPCAM} -o /tmp/${STAMP}.jpg && \
+  #  curl -s -k -u "${YPASS}" -T "/tmp/${STAMP}.jpg" https://webdav.yandex.ru/Public/${STAMP}.jpg && \
+  #  rm -f /tmp/${STAMP}.jpg \
+  #) &
+  #
+  sleep ${SLEEP}
+  #
 fi
