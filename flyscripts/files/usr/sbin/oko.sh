2f22d9061efe6706fd32595ba5ec0dacd5ebce0a
Zig Fisher <flyrouter@gmail.com>
2017-08-27 12:42:26 +0300

    Update

--- flyscripts/files/usr/sbin/oko.sh
+++ flyscripts/files/usr/sbin/oko.sh
@@ -7,7 +7,7 @@
 IPCAM='http://192.168.1.10/webcapture.jpg?command=snap&channel=1'
 TOKEN='367706172:AAHqXLtPAUExiF_CUPe42SvrK9b7csi953A'
 RUPOR='@Bricket1'
-ALARM='RadarDetect!'
+ALARM='RadarDetect:'
 YPASS='user:pass'
 DELAY='60'
 SLEEP='1'
@@ -20,7 +20,7 @@ if [ "`usbgpio statusin | awk -F ' ' '/LED 7/ {print $4}'`" = "1" ] ; then
   last=`cat /tmp/.delay`
   curr=`date +%s`
   [ $(($curr-$last)) -gt ${DELAY} ] && \
-    curl -s -k -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${RUPOR}&text=${ALARM}" && date +%s >/tmp/.delay
+    curl -s -k -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${RUPOR}&text=${ALARM}${STAMP}" && date +%s >/tmp/.delay
   #
   # Send photo to Telegram channel
   ( curl -s -k ${IPCAM} -o /tmp/${STAMP}.jpg && \
