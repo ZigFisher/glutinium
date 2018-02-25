d1847e4c75c37064cd0ebb6bb3321cb823a12b7c
Igor Zalatov <zig@dolphin.flymon.net>
2018-02-25 21:19:41 +0300

    Update

--- tg-bot1/files/usr/bin/tg_snap.sh
+++ tg-bot1/files/usr/bin/tg_snap.sh
@@ -2,14 +2,14 @@
 #
 # Exec /snap command
 
-sysid=`uci get system.@system[0].hostname`
-stamp=`date +%Y%m%d%H%M%S`
-token=`uci get telegram.bot.token`
-rupor=`uci get telegram.bot.rupor`
-ipcam="http://10.255.10.247/webcapture.jpg?command=snap&channel=1"
+ipcam=$(uci get telegram.bot.ipcam)
+rupor=$(uci get telegram.bot.rupor)
+stamp=$(date +%Y.%m.%d_%H:%M:%S)
+sysid=$(uci get system.@system[0].hostname)
+token=$(uci get telegram.bot.token)
 
 
 curl -s -k ${ipcam} -o /tmp/snap.jpg && \
   curl -s -k -X POST "https://api.telegram.org/bot${token}/sendPhoto?chat_id=${rupor}" -H "Content-Type: multipart/form-data" \
-    -F "photo=@/tmp/snap.jpg" -F "caption=${sysid} - TimeStamp: ${stamp}" >/dev/null 2>&1 && \
+    -F "photo=@/tmp/snap.jpg" -F "caption=${sysid} - ${stamp}" >/dev/null 2>&1 && \
   rm -rf /tmp/snap.jpg
