3df97c3950a92e287e2bae5a24a2f6cee4e5126f
Igor Zalatov <zig@dolphin.flymon.net>
2018-02-27 02:08:33 +0300

    Update sirena script

--- flyscripts/files/usr/sbin/sirena.sh
+++ flyscripts/files/usr/sbin/sirena.sh
@@ -1,18 +1,32 @@
-!/bin/sh
+#!/bin/sh
 #
-# Scan home for sitena alarm
+# Scan home for sirena alarm
 
-DELAY='10'
+delay='5'
 
-if [ "`usbgpio statusin | awk -F ' ' '/LED 3/ {print $4}'`" = "1" ] ; then
+if [ "`usbgpio statusin | awk -F ' ' '/LED 8/ {print $4}'`" = "1" ] ; then
   #
-  usbgpio on 4 && logger -t sirena "Sirena ON".....
-  sleep ${DELAY}
-  usbgpio off 4 && logger -t sirena "Sirena OFF"
+  logger -t sirena "Door open"
   #
+  if [ -f /tmp/sirena.run ]; then
+    logger -t "Sirena trigger"
+  else
+    (oko1.sh Door open && tg_snap.sh && tg_snap.sh && tg_snap.sh) &
+    usbgpio on 4 && logger -t sirena "Sirena on"
+    sleep ${delay}
+    usbgpio off 4 && logger -t sirena "Sirena off"
+    echo "Trigger" >/tmp/sirena.run
+  fi
+  #
+else
+  if [ -f /tmp/sirena.run ]; then
+    (oko1.sh Door closed && tg_snap.sh) &
+    rm -f /tmp/sirena.run
+  fi
+  #logger -t sirena "Door closed"
+  echo "Waiting"
 fi
-
 
 #
 # Alarm Buzzer Test
-#usbgpio mode 3 1; (while true ; do sleep 1; /usr/sbin/sirena.sh >/dev/null 2>&1; done) &
+#usbgpio mode 3 1; (while true ; do sleep 2; /usr/sbin/sirena.sh >/dev/null 2>&1; done) &
