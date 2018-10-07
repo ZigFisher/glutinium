b0ae8eed9207abc8af68b8c38450865a265ea4cd
Igor Zalatov <zig@dolphin.flymon.net>
2018-10-08 01:13:56 +0300

    Update

--- luci-mod-openwisp/luasrc/controller/openwisp/settings.lua
+++
@@ -1,8 +0,0 @@
--- OpenWISP Project (GPLv3) http://openwisp.org
-
-module("luci.controller.openwisp.settings", package.seeall)
-require("uci")
-
-function index()
-	entry({"openwisp", "network"}, cbi("openwisp/network", {autoapply=true}), _("Network"), 21)
-end
