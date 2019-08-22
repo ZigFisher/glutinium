0842061f440a5f7a756640b60ac3ed6c15b556fc
Igor Zalatov <zig@dolphin.flymon.net>
2019-08-22 22:14:54 +0300

    Update script

--- ipcam/files/shell/tg_snap.sh
+++ ipcam/files/shell/tg_snap.sh
@@ -13,10 +13,17 @@ if [ -f ${IPC} ]; then
     do local ${settings}
   done < ${IPC}
   #
-  stamp=$(date +%Y.%m.%d_%H:%M:%S)
+  if [ ${socks5_enable} = 1 ]; then
+    curl_options="-s -k --socks5-hostname ${socks5_server}:${socks5_port} --proxy-user ${socks5_login}:${socks5_password}"
+  else
+    curl_options="-s -k"
+  fi
   #
-  curl -s -k "http://127.0.0.1/webcapture.jpg?command=snap&channel=1" -o ${TMP}/snap.jpg && \
-    curl -s -k -X POST "https://api.telegram.org/bot${telegram_token}/sendPhoto?chat_id=${telegram_group}" -H "Content-Type: multipart/form-data" \
+  stamp=$(date +%Y.%m.%d_%H:%M:%S)
+  PASSW=$(cat /mnt/mtd/Config/Account1 | jsonfilter -e '@.Users[@.Name="admin"].Password')
+  #
+  curl -s -k "http://127.0.0.1/webcapture.jpg?command=snap&channel=1&user=admin&password=${PASSW}" -o ${TMP}/snap.jpg && \
+    curl ${curl_options} -X POST "https://api.telegram.org/bot${telegram_token}/sendPhoto?chat_id=${telegram_group}" -H "Content-Type: multipart/form-data" \
       -F "photo=@/tmp/snap.jpg" -F "caption=${device_name} - ${stamp}" >/dev/null 2>&1 && \
     rm -rf ${TMP}/snap.jpg
   #
